e214e3962ba061698203a161059e724e9ecf39c4
Jason Martinez <jasonmartinez@JMarz-MacBook-Pro.local>
2025-07-07 18:13:52 -0500

    Initial terraform plan worked now going to test cicd pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,12 +21,12 @@ resource "aws_vpc" "main" {
 module "web" {
   source = "./modules/web"
 
-  vpc_id             = aws_vpc.main.id
-  web_subnet_cidr_a  = "192.168.11.0/24"
-  web_subnet_cidr_b  = "192.168.12.0/24"
-  az_a               = "us-east-2a"
-  az_b               = "us-east-2b"
-  env                = var.env
+  vpc_id            = aws_vpc.main.id
+  web_subnet_cidr_a = "192.168.11.0/24"
+  web_subnet_cidr_b = "192.168.12.0/24"
+  az_a              = "us-east-2a"
+  az_b              = "us-east-2b"
+  env               = var.env
 }
 
 #########################################
@@ -35,20 +35,20 @@ module "web" {
 module "app" {
   source = "./modules/app"
 
-  vpc_id             = aws_vpc.main.id
-  app_subnet_cidr_a  = "192.168.21.0/24"
-  app_subnet_cidr_b  = "192.168.22.0/24"
-  nat_gateway_id     = module.web.nat_gateway_id  # TO BE ADDED TO WEB OUTPUTS
-  alb_sg_id          = module.web.alb_sg_id
-  az_a               = "us-east-2a"
-  az_b               = "us-east-2b"
-  env                = var.env
+  vpc_id            = aws_vpc.main.id
+  app_subnet_cidr_a = "192.168.21.0/24"
+  app_subnet_cidr_b = "192.168.22.0/24"
+  nat_gateway_id    = module.web.nat_gateway_id # TO BE ADDED TO WEB OUTPUTS
+  alb_sg_id         = module.web.alb_sg_id
+  az_a              = "us-east-2a"
+  az_b              = "us-east-2b"
+  env               = var.env
 
-  ami_id             = var.ami_id
-  instance_type      = var.instance_type
-  min_size           = var.min_size
-  max_size           = var.max_size
-  desired_capacity   = var.desired_capacity
+  ami_id           = var.ami_id
+  instance_type    = var.instance_type
+  min_size         = var.min_size
+  max_size         = var.max_size
+  desired_capacity = var.desired_capacity
 }
 
 ################################
@@ -57,33 +57,33 @@ module "app" {
 module "db" {
   source = "./modules/db"
 
-  vpc_id             = aws_vpc.main.id
-  az_a               = "us-east-2a"
-  az_b               = "us-east-2b"
-  env                = var.env
+  vpc_id = aws_vpc.main.id
+  az_a   = "us-east-2a"
+  az_b   = "us-east-2b"
+  env    = var.env
 
-  db_subnet_cidr_a   = "192.168.31.0/24"
-  db_subnet_cidr_b   = "192.168.32.0/24"
+  db_subnet_cidr_a = "192.168.31.0/24"
+  db_subnet_cidr_b = "192.168.32.0/24"
 
-  app_sg_id          = module.app.app_sg_id
+  app_sg_id = module.app.app_sg_id
 
-  engine             = var.db_engine
-  engine_version     = var.db_engine_version
-  instance_class     = var.db_instance_class
-  allocated_storage  = var.db_allocated_storage
-  db_name            = var.db_name
-  db_username        = var.db_username
-  db_password        = var.db_password
-  db_port            = var.db_port
+  engine            = var.db_engine
+  engine_version    = var.db_engine_version
+  instance_class    = var.db_instance_class
+  allocated_storage = var.db_allocated_storage
+  db_name           = var.db_name
+  db_username       = var.db_username
+  db_password       = var.db_password
+  db_port           = var.db_port
 }
 
 module "security" {
   source = "./modules/security"
 
-  vpc_id           = aws_vpc.main.id
-  env              = var.env
-  ssh_cidr_blocks  = ["0.0.0.0/0"]
-  web_subnet_ids   = module.web.web_subnet_ids
-  app_subnet_ids   = module.app.app_subnet_ids
-  web_subnet_cidr  = "192.168.11.0/24" # or combine 11.0/24 + 12.0/24 if needed
+  vpc_id          = aws_vpc.main.id
+  env             = var.env
+  ssh_cidr_blocks = ["0.0.0.0/0"]
+  web_subnet_ids  = module.web.web_subnet_ids
+  app_subnet_ids  = module.app.app_subnet_ids
+  web_subnet_cidr = "192.168.11.0/24" # or combine 11.0/24 + 12.0/24 if needed
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -30,40 +30,40 @@ variable "desired_capacity" {
 }
 
 variable "db_engine" {
-  type        = string
-  default     = "mysql"
+  type    = string
+  default = "mysql"
 }
 
 variable "db_engine_version" {
-  type        = string
-  default     = "8.0"
+  type    = string
+  default = "8.0"
 }
 
 variable "db_instance_class" {
-  type        = string
-  default     = "db.t3.micro"
+  type    = string
+  default = "db.t3.micro"
 }
 
 variable "db_allocated_storage" {
-  type        = number
-  default     = 20
+  type    = number
+  default = 20
 }
 
 variable "db_name" {
-  type        = string
-  default     = "appdb"
+  type    = string
+  default = "appdb"
 }
 
 variable "db_username" {
-  type        = string
+  type = string
 }
 
 variable "db_password" {
-  type        = string
-  sensitive   = true
+  type      = string
+  sensitive = true
 }
 
 variable "db_port" {
-  type        = number
-  default     = 3306
+  type    = number
+  default = 3306
 }
