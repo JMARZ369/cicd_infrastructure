ac394ed9fbab795f99a6d7970113dc113fafdf27
Jason Martinez <jasonmartinez@JMarz-MacBook-Pro.local>
2025-07-08 21:18:00 -0500

    fixed formating on main.tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,18 +21,18 @@ resource "aws_vpc" "main" {
 module "web" {
   source = "./modules/web"
 
-  vpc_id             = aws_vpc.main.id
-  web_subnet_cidr_a  = var.web_subnet_cidr_a
-  web_subnet_cidr_b  = var.web_subnet_cidr_b
-  az_a               = var.az_a
-  az_b               = var.az_b
-  env                = var.env
+  vpc_id            = aws_vpc.main.id
+  web_subnet_cidr_a = var.web_subnet_cidr_a
+  web_subnet_cidr_b = var.web_subnet_cidr_b
+  az_a              = var.az_a
+  az_b              = var.az_b
+  env               = var.env
 
-  ami_id             = var.ami_id
-  instance_type      = var.instance_type
-  min_size           = var.min_size
-  max_size           = var.max_size
-  desired_capacity   = var.desired_capacity
+  ami_id           = var.ami_id
+  instance_type    = var.instance_type
+  min_size         = var.min_size
+  max_size         = var.max_size
+  desired_capacity = var.desired_capacity
 }
 
 ##############################
@@ -41,20 +41,20 @@ module "web" {
 module "app" {
   source = "./modules/app"
 
-  vpc_id             = aws_vpc.main.id
-  app_subnet_cidr_a  = var.app_subnet_cidr_a
-  app_subnet_cidr_b  = var.app_subnet_cidr_b
-  nat_gateway_id     = module.web.nat_gateway_id
-  alb_sg_id          = module.web.alb_sg_id
-  az_a               = var.az_a
-  az_b               = var.az_b
-  env                = var.env
+  vpc_id            = aws_vpc.main.id
+  app_subnet_cidr_a = var.app_subnet_cidr_a
+  app_subnet_cidr_b = var.app_subnet_cidr_b
+  nat_gateway_id    = module.web.nat_gateway_id
+  alb_sg_id         = module.web.alb_sg_id
+  az_a              = var.az_a
+  az_b              = var.az_b
+  env               = var.env
 
-  ami_id             = var.ami_id
-  instance_type      = var.instance_type
-  min_size           = var.min_size
-  max_size           = var.max_size
-  desired_capacity   = var.desired_capacity
+  ami_id           = var.ami_id
+  instance_type    = var.instance_type
+  min_size         = var.min_size
+  max_size         = var.max_size
+  desired_capacity = var.desired_capacity
 }
 
 ##############################
@@ -63,23 +63,23 @@ module "app" {
 module "db" {
   source = "./modules/db"
 
-  vpc_id             = aws_vpc.main.id
-  db_subnet_cidr_a   = var.db_subnet_cidr_a
-  db_subnet_cidr_b   = var.db_subnet_cidr_b
-  az_a               = var.az_a
-  az_b               = var.az_b
-  env                = var.env
+  vpc_id           = aws_vpc.main.id
+  db_subnet_cidr_a = var.db_subnet_cidr_a
+  db_subnet_cidr_b = var.db_subnet_cidr_b
+  az_a             = var.az_a
+  az_b             = var.az_b
+  env              = var.env
 
-  app_sg_id          = module.app.app_sg_id
+  app_sg_id = module.app.app_sg_id
 
-  engine             = var.db_engine
-  engine_version     = var.db_engine_version
-  instance_class     = var.db_instance_class
-  allocated_storage  = var.db_allocated_storage
-  db_name            = var.db_name
-  db_username        = var.db_username
-  db_password        = var.db_password
-  db_port            = var.db_port
+  engine            = var.db_engine
+  engine_version    = var.db_engine_version
+  instance_class    = var.db_instance_class
+  allocated_storage = var.db_allocated_storage
+  db_name           = var.db_name
+  db_username       = var.db_username
+  db_password       = var.db_password
+  db_port           = var.db_port
 }
 
 ##############################
@@ -88,10 +88,10 @@ module "db" {
 module "security" {
   source = "./modules/security"
 
-  vpc_id           = aws_vpc.main.id
-  env              = var.env
-  ssh_cidr_blocks  = ["0.0.0.0/0"]  # Replace this with a secure /32 IP
-  web_subnet_ids   = module.web.web_subnet_ids
-  app_subnet_ids   = module.app.app_subnet_ids
-  web_subnet_cidr  = var.web_subnet_cidr_a
+  vpc_id          = aws_vpc.main.id
+  env             = var.env
+  ssh_cidr_blocks = ["0.0.0.0/0"] # Replace this with a secure /32 IP
+  web_subnet_ids  = module.web.web_subnet_ids
+  app_subnet_ids  = module.app.app_subnet_ids
+  web_subnet_cidr = var.web_subnet_cidr_a
 }
